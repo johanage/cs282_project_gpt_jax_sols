546acae03f5fb3266b3d8f0be31e619e4523d5aa
shreyash2106 <shreyash2106@berkeley.edu>
2023-04-16 12:37:01 -0700

    initial code for training in jax

--- trainer.py
+++ trainer.py
@@ -4,109 +4,55 @@ Simple training loop; Boilerplate that could apply to any arbitrary neural netwo
 so nothing in this file really has anything to do with GPT specifically.
 """
 
-import time
-from collections import defaultdict
+import jax
+import jax.numpy as jnp
+import haiku as hk
+
+from jax.experimental import optimizers
+import optax
+from optax import chain, clip_by_global_norm, scale_by_adam, scale, scale_by_schedule, add_decayed_weights
+from jax import local_device_count
+
+from tqdm import tqdm
+import math
+import numpy as np
+from typing import Mapping
+import functools
+from functools import partial
+import pickle 
 
 import torch
-from torch.utils.data.dataloader import DataLoader
-#from mingpt.utils import CfgNode as CN
+from torch.utils.data import Dataset, DataLoader
 
-CN = None
+from train_config import *
 
 class Trainer:
-
-    @staticmethod
-    def get_default_config():
-        C = CN()
-        # device to train on
-        C.device = 'auto'
-        # dataloder parameters
-        C.num_workers = 4
-        # optimizer parameters
-        C.max_iters = None
-        C.batch_size = 64
-        C.learning_rate = 3e-4
-        C.betas = (0.9, 0.95)
-        C.weight_decay = 0.1 # only applied on matmul weights
-        C.grad_norm_clip = 1.0
-        return C
-
-    def __init__(self, config, model, train_dataset):
-        self.config = config
-        self.model = model
-        self.optimizer = None
+    def __init__(self, loss_fn, train_dataset, test_dataset):
+        self.loss_fn = loss_fn
         self.train_dataset = train_dataset
-        self.callbacks = defaultdict(list)
+        self.test_dataset = test_dataset
 
-        # determine the device we'll train on
-        if config.device == 'auto':
-            self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
-        else:
-            self.device = config.device
-        self.model = self.model.to(self.device)
-        print("running on device", self.device)
+    def get_training_params(self):
+        prng, subkey = jax.random.split(prng)
+        train_dl = DataLoader(self.train_dataset, batch_size=batch_size, num_workers=num_workers)
+        batch = next(iter(train_dl))
+        x_batch, y_batch = map(jnp.array, batch)
+        params = self.loss_fn.init(subkey, x_batch, y_batch)
+        return params
 
-        # variables that will be assigned to trainer class later for logging and etc
-        self.iter_num = 0
-        self.iter_time = 0.0
-        self.iter_dt = 0.0
+    def run_trainer(self):
+        params = self.get_training_params()
+        optimizer = optax.adamw(learning_rate=learning_rate, b1=beta1, b2=beta2, weight_decay=weight_decay)
+        loss_fn = self.loss_fn.apply
+        it = 0 # counter used for learning rate decay
+        for epoch in range(max_epochs):
+            params, opt_state, it = self.run_epoch(params, opt_state, it, 'train')
+            if self.test_dataset is not None:
+                test_loss = self.run_epoch(params, opt_state, 0, 'test')
 
-    def add_callback(self, onevent: str, callback):
-        self.callbacks[onevent].append(callback)
+    def run_epoch(self, params, opt_state, it, mode):
+        dataset = self.train_dataset if mode == 'train' else self.test_dataset
+        loader = DataLoader(dataset, shuffle=True, pin_memory=True, batch_size=batch_size, num_workers=num_workers)
+        losses = []
 
-    def set_callback(self, onevent: str, callback):
-        self.callbacks[onevent] = [callback]
 
-    def trigger_callbacks(self, onevent: str):
-        for callback in self.callbacks.get(onevent, []):
-            callback(self)
-
-    def run(self):
-        model, config = self.model, self.config
-
-        # setup the optimizer
-        self.optimizer = model.configure_optimizers(config)
-
-        # setup the dataloader
-        train_loader = DataLoader(
-            self.train_dataset,
-            sampler=torch.utils.data.RandomSampler(self.train_dataset, replacement=True, num_samples=int(1e10)),
-            shuffle=False,
-            pin_memory=True,
-            batch_size=config.batch_size,
-            num_workers=config.num_workers,
-        )
-
-        model.train()
-        self.iter_num = 0
-        self.iter_time = time.time()
-        data_iter = iter(train_loader)
-        while True:
-
-            # fetch the next batch (x, y) and re-init iterator if needed
-            try:
-                batch = next(data_iter)
-            except StopIteration:
-                data_iter = iter(train_loader)
-                batch = next(data_iter)
-            batch = [t.to(self.device) for t in batch]
-            x, y = batch
-
-            # forward the model
-            logits, self.loss = model(x, y)
-
-            # backprop and update the parameters
-            model.zero_grad(set_to_none=True)
-            self.loss.backward()
-            torch.nn.utils.clip_grad_norm_(model.parameters(), config.grad_norm_clip)
-            self.optimizer.step()
-
-            self.trigger_callbacks('on_batch_end')
-            self.iter_num += 1
-            tnow = time.time()
-            self.iter_dt = tnow - self.iter_time
-            self.iter_time = tnow
-
-            # termination conditions
-            if config.max_iters is not None and self.iter_num >= config.max_iters:
-                break
