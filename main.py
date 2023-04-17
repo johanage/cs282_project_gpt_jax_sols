7ef3381a093adb278114063f3f9a675f65547c91
johanage <j.f.agerup@fys.uio.no>
2023-04-17 22:26:23 +0200

    first draft of code running with mask and dropout

--- main.py
+++ main.py
@@ -2,18 +2,17 @@ from jax import lax, random, numpy as jnp
 import jax
 from flax.core import freeze, unfreeze
 from flax import linen as nn
-
-from model.model import LinearBlock, CausalSelfAttention, Block
-
+from model import LinearBlock, CausalSelfAttention, Block
 key1, key2, dropout_key = random.split(random.PRNGKey(0), 3)
 print(key1, key2)
 x = random.uniform(key1, (4, 10, 21))
+print(" type of x is : ", type(x))
 model = Block(n_head=7, n_embd=3, sequence_length=10) #CausalSelfAttention(n_head=7, n_embd=3, sequence_length=10)
 params = model.init(key2, x)
-
-y = model.apply(params, x)
-
-
+# for the dropout to work a rng key with prng_collective (name) ´dropout´ 
+# has to be added to the arguments as a dictionary with key prng_collective and 
+# value the prng key
+y = model.apply(params, x, rngs = {'dropout' : dropout_key})
 print('initialized parameter shapes:\n', jax.tree_util.tree_map(jnp.shape, unfreeze(params)))
 print('output:\n', y)
 print(f"Output shape: {y.shape}")
--- model.py
+++ model.py
@@ -3,72 +3,106 @@ from jax import lax, random, numpy as jnp
 
 
 class LinearBlock(nn.Module):
-    dense1_features: int
-    dense2_features: int
+	dense1_features: int
+	dense2_features: int
 
-    def setup(self):
-        self.dense1 = nn.Dense(self.dense1_features)
-        self.dense2 = nn.Dense(self.dense2_features)
+	def setup(self):
+		self.dense1 = nn.Dense(self.dense1_features)
+		self.dense2 = nn.Dense(self.dense2_features)
 
-    def __call__(self, x):
-        x = self.dense1(x)
-        x = nn.gelu(x)
-        x = self.dense2(x)
-        return x
+	def __call__(self, x):
+		x = self.dense1(x)
+		x = nn.gelu(x)
+		x = self.dense2(x)
+		return x
 
 
 class CausalSelfAttention(nn.Module):
-    n_head: int
-    n_embd: int
-    sequence_length: int
+	n_head: int
+	n_embd: int
+	sequence_length: int
+	train : bool = False
+	
+	def setup(self, do_rate = 0.3, block_size = None):
+		"""
+		Sets up the attention block
+		do_rate - dropout probability, float
+		
+		"""
+		total_number_of_features = self.n_embd * self.n_head
+		self.qdense = nn.Dense(total_number_of_features)
+		self.vdense = nn.Dense(total_number_of_features)
+		self.kdense = nn.Dense(total_number_of_features)
+		self.attn_dropout = nn.Dropout(rate = do_rate)
+		if block_size is None:
+			self.block_size = self.sequence_length
+		else: self.block_size = block_size
+		# this should be buffered such that update under SGD is avoided
+		# see nn.Module.register_buffer for reference
+		mask_inner = jnp.tril(jnp.ones((self.sequence_length, self.sequence_length)))
+		self.mask = lax.broadcast(mask_inner, (self.n_embd+1, self.n_head)) #,self.sequence_length, self.sequence_length))
 
-    def setup(self):
-        total_number_of_features = self.n_embd * self.n_head
-        self.qdense = nn.Dense(total_number_of_features)
-        self.vdense = nn.Dense(total_number_of_features)
-        self.kdense = nn.Dense(total_number_of_features)
-        self.mask = jnp.tril(jnp.ones((self.sequence_length, self.sequence_length)))
-        self.attn_dropout = nn.Dropout(rate = 0.3)
 
-    def __call__(self, x):
-        # TODO: DROPOUT
-        # TODO: MASKING
-        batch_size, sequence_length, _ = x.shape
-        q, v, k = self.qdense(x), self.vdense(x), self.kdense(x)
-        q = q.reshape(batch_size, sequence_length, self.n_head, self.n_embd).transpose(0, 2, 1, 3)
-        v = v.reshape(batch_size, sequence_length, self.n_head, self.n_embd).transpose(0, 2, 1, 3)
-        k = k.reshape(batch_size, sequence_length, self.n_head, self.n_embd).transpose(0, 2, 1, 3)
+	def __call__(self, x):
+		"""
+		Args:
+		x    - input, torch tensor
+		"""
+		batch_size, sequence_length, _ = x.shape
+		q, v, k = self.qdense(x), self.vdense(x), self.kdense(x)
+		q = q.reshape(batch_size, sequence_length, self.n_head, self.n_embd).transpose(0, 2, 1, 3)
+		v = v.reshape(batch_size, sequence_length, self.n_head, self.n_embd).transpose(0, 2, 1, 3)
+		k = k.reshape(batch_size, sequence_length, self.n_head, self.n_embd).transpose(0, 2, 1, 3)
 
-        att = (q @ k.transpose(0, 1, 3, 2)) * (1 / jnp.sqrt(self.n_embd))
+		att = (q @ k.transpose(0, 1, 3, 2)) * (1 / jnp.sqrt(self.n_embd))
 
-        # att = lax.select(self.mask==0, att, lax.broadcast(-jnp.inf, att.shape))
+		# masking :  att = att.masked_fill(self.bias[:,:,:T,:T] == 0, float('-inf'))
+		# replacing Tensor.masked_fill(mask, value)
+		# mask  - boolean mask
+		# value - the vbalue to fill in with
+		# jax.lax.select(pred, on_true, on_false)
+		minfs = lax.broadcast(-jnp.inf, att.shape)
+		print("shape minfs : ", minfs.shape, "shape mask : ", self.mask.shape)
+		att = lax.select(self.mask[:,:,:sequence_length,:sequence_length] == 0, att, minfs)
 
-        att = nn.softmax(att, axis=3)
+		# apply softmax
+		att = nn.softmax(att, axis=3)
 
-        #att = self.attn_dropout(att, deterministic=not training, rngs=rngs)
-        y = att @ v
-        y = y.transpose(0,2,1,3)
-        # MISSING: making y contiguous
-        y = y.reshape((batch_size, sequence_length, self.n_embd*self.n_head))
-        return y
+		# the manual way
+		#key = random.PRNGKey(2023) # can/should be changed
+		#dropout = random.bernoulli(key, p = p_do, shape = att.size())
+		#att_do = att * dropout
+
+		# using the nn module and jax
+		# possibly use module.apply to get full jax functionality
+		# may have to inherit something from a flax/jax module class
+		att = self.attn_dropout(att, deterministic = not self.train)
+
+		y = att @ v
+		y = y.transpose(0,2,1,3)
+		# MISSING: making y contiguous
+		y = y.reshape((batch_size, sequence_length, self.n_embd*self.n_head))
+		return y
 
 class Block(nn.Module):
-    n_head: int
-    n_embd: int
-    sequence_length: int
+	n_head: int
+	n_embd: int
+	sequence_length: int
 
-    def setup(self) -> None:
-        # TODO: DROPOUT
-        self.ln_1 = nn.LayerNorm(self.n_embd)
-        self.attn = CausalSelfAttention(n_head=self.n_head, n_embd=self.n_embd, sequence_length=self.sequence_length)
-        self.ln_2 = nn.LayerNorm(self.n_embd)
-        self.fc = nn.Dense(self.n_embd*self.n_head*4)
-        self.c_project = nn.Dense(self.n_embd*self.n_head)
-        self.act = nn.gelu
+	def setup(self) -> None:
+		# TODO: DROPOUT
+		self.ln_1 = nn.LayerNorm(self.n_embd)
+		self.attn = CausalSelfAttention(n_head=self.n_head, 
+										n_embd=self.n_embd, 
+										sequence_length=self.sequence_length)
+		self.ln_2 = nn.LayerNorm(self.n_embd)
+		self.fc = nn.Dense(self.n_embd*self.n_head*4)
+		self.c_project = nn.Dense(self.n_embd*self.n_head)
+		self.act = nn.gelu
 
-    def __call__(self, x):
-        attention_output = x + self.attn(self.ln_1(x))
-        # TODO Dropout
-        mlp_output = attention_output + self.c_project(self.act(self.fc(attention_output)))
+	def __call__(self, x):
+		attention_output = x + self.attn(self.ln_1(x))
+		# TODO Dropout
+		mlp_output = attention_output + self.c_project(self.act(self.fc(attention_output)))
 
-        return mlp_output
+		return mlp_output
