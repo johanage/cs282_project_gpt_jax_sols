95d3c3b6bfb8dfbbafb7d5f39dd4f09648f077d8
johanage <j.f.agerup@fys.uio.no>
2023-04-19 02:37:35 +0200

    Copying bias and weights succeeded but does not pass correctness check... Some dimensions in the MLP were inverted (the FC and projection weights) - this could be the reason for the error

--- test_set_params.py
+++ test_set_params.py
@@ -92,8 +92,35 @@ print("Flattened parameter tree: \n", jax.tree_util.tree_map(jnp.shape, flat_par
 gpt_param_dict = {}
 for name, param in model.named_parameters():
     if param.requires_grad:
-        gpt_param_dict[name] = param
-print("GPT dict of named parameters that req grad: \n",  list(gpt_param_dict.keys()) )
+        # store the params as jax numpy arrays in the dict
+        gpt_param_dict[name] = jnp.array(param.detach().numpy())
+print("GPT dict of named parameters that req grad: \n",list(gpt_param_dict.keys()) )
+
+flat_params['params/wte/embedding'] = gpt_param_dict['transformer.wte.weight']
+flat_params['params/wpe/embedding'] = gpt_param_dict['transformer.wpe.weight']
+flat_params['params/blocks_0/ln_1/scale'] = gpt_param_dict['transformer.h.0.ln_1.weight']
+flat_params['params/blocks_0/ln_1/bias'] = gpt_param_dict['transformer.h.0.ln_1.bias']
+att_w = model.get_parameter('transformer.h.0.attn.c_attn.weight')
+att_b = model.get_parameter('transformer.h.0.attn.c_attn.bias')
+q_w, k_w, v_w = att_w.split(att_w.size()[-1], dim=0)
+q_b, k_b, v_b = att_b.split(att_w.size()[-1], dim=0)
+flat_params['params/blocks_0/attn/qdense/kernel']    = jnp.array(q_w.detach().numpy())
+flat_params['params/blocks_0/attn/qdense/bias']      = jnp.array(q_b.detach().numpy())
+flat_params['params/blocks_0/attn/vdense/kernel']    = jnp.array(v_w.detach().numpy())
+flat_params['params/blocks_0/attn/vdense/bias']      = jnp.array(v_b.detach().numpy())
+flat_params['params/blocks_0/attn/kdense/kernel']    = jnp.array(k_w.detach().numpy())
+flat_params['params/blocks_0/attn/kdense/bias']      = jnp.array(k_b.detach().numpy())
+flat_params['params/blocks_0/attn/c_proj/kernel']    = gpt_param_dict['transformer.h.0.attn.c_proj.weight']
+flat_params['params/blocks_0/attn/c_proj/bias']      = gpt_param_dict['transformer.h.0.attn.c_proj.bias']
+flat_params['params/blocks_0/ln_2/scale']            = gpt_param_dict['transformer.h.0.ln_2.weight']
+flat_params['params/blocks_0/ln_2/bias']             = gpt_param_dict['transformer.h.0.ln_2.bias']
+flat_params['params/blocks_0/mlpf/fc/kernel']        = gpt_param_dict['transformer.h.0.mlp.c_fc.weight'].T
+flat_params['params/blocks_0/mlpf/fc/bias']          = gpt_param_dict['transformer.h.0.mlp.c_fc.bias']
+flat_params['params/blocks_0/mlpf/c_project/kernel'] = gpt_param_dict['transformer.h.0.mlp.c_proj.weight'].T
+flat_params['params/blocks_0/mlpf/c_project/bias']   = gpt_param_dict['transformer.h.0.mlp.c_proj.bias']
+flat_params['params/ln_f/scale']                     = gpt_param_dict['transformer.ln_f.weight']
+flat_params['params/ln_f/bias']                      = gpt_param_dict['transformer.ln_f.bias']
+flat_params['params/lm_head/kernel']                 = gpt_param_dict['lm_head.weight'].T
 
 
 # Unflatten.
@@ -109,9 +136,10 @@ Here we produce output and check for equivalence between the two implementations
 """
 
 # out jax implementation
-y = model_jax.apply(params_jax, x, rngs = {'dropout' : dropout_key})
+#y = model_jax.apply(params_jax, x, rngs = {'dropout' : dropout_key})
+y = model_jax.apply(unflat_params, x, rngs = {'dropout' : dropout_key})
 
 # out OG gpt
 y_gpt = model(torch.tensor(x.tolist()))
 
-
+#print(y == y_gpt)
