b334a793854a452d83ef0a0707ed7d8f347aacc7
shreyash2106 <shreyash2106@berkeley.edu>
2023-04-21 13:49:55 -0700

    public config and batch size

--- tests/test_mlp.py
+++ tests/test_mlp.py
@@ -5,6 +5,17 @@ from flax import linen as nn, traverse_util
 
 from model import GPT as GPT_jax
 
+BATCH_SIZE = 4
+config_jax = {
+    "n_layers": 1,
+    "n_head": 7,
+    "n_embd": 21,
+    "vocab_size": 10,
+    "block_size": 10,
+    "embd_pdrop": 0.1,
+    "train": True
+}
+
 def test_mlp():
     key1, key2, dropout_key = random.split(random.PRNGKey(1), 3)
     x = random.randint(key1, (BATCH_SIZE, config_jax["block_size"]), 0, config_jax["vocab_size"])
