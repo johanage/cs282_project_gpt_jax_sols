94bf8a3826ff0770341e00565b371547ef4cc611
shreyash2106 <shreyash2106@berkeley.edu>
2023-04-21 15:53:48 -0700

    import model configurations from config file

--- tests/test_set_params.py
+++ tests/test_set_params.py
@@ -19,16 +19,7 @@ from flax import linen as nn, traverse_util
 
 from model import GPT as GPT_jax
 
-BATCH_SIZE = 4
-config_jax = {
-    "n_layers": 1,
-    "n_head": 7,
-    "n_embd": 21,
-    "vocab_size": 10,
-    "block_size": 10,
-    "embd_pdrop": 0.1,
-    "train": True
-}
+from config import config_gpt, config_jax, BATCH_SIZE
 
 key1, key2, dropout_key = random.split(random.PRNGKey(1), 3)
 x = random.randint(key1, (BATCH_SIZE, config_jax["block_size"]), 0, config_jax["vocab_size"])
@@ -37,21 +28,6 @@ params_jax = model_jax.init({"params": key2, 'dropout' : dropout_key}, x)
 param_count = sum(x.size for x in jax.tree_leaves(params_jax))
 print("Number of parameters: ", param_count)
 print('initialized parameter shapes:\n', jax.tree_util.tree_map(lambda x: x.shape, params_jax)) # Checking output shapes
-
-
-config_gpt = {
-    "n_layer": 1,
-    "n_head": 7,
-    "n_embd": 21,
-    "sequence_length": 10,
-    "vocab_size": 10,
-    "block_size": 10,
-    "embd_pdrop": 0.1,
-    "resid_pdrop": 0.1,
-    "attn_pdrop": 0.1,
-    "train": True,
-	"model_type" : None
-}
 
 def count_parameters(model, full=False):
     if full:
