71cae282cfc1ec2a98674d09d31d5699e68be5ba
shreyash2106 <shreyash2106@berkeley.edu>
2023-04-21 15:52:26 -0700

    model configurations for tests

---
+++ tests/config.py
@@ -0,0 +1,24 @@
+config_gpt = {
+    "n_layer": 1,
+    "n_head": 7,
+    "n_embd": 21,
+    "sequence_length": 10,
+    "vocab_size": 10,
+    "block_size": 10,
+    "embd_pdrop": 0.1,
+    "resid_pdrop": 0.1,
+    "attn_pdrop": 0.1,
+    "train": True,
+	"model_type" : None
+}
+
+BATCH_SIZE = 4
+config_jax = {
+    "n_layers": 1,
+    "n_head": 7,
+    "n_embd": 21,
+    "vocab_size": 10,
+    "block_size": 10,
+    "embd_pdrop": 0.1,
+    "train": True
+}
