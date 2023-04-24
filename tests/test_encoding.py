7208a1c67169db0062334908c5c826c9f2d4b04c
Avikam Chauhan <avikam-chauhan@users.noreply.github.com>
2023-04-23 19:41:09 -0700

    Removed calls to solution code from encoder/decoder test cases

--- tests/test_encoding.py
+++ tests/test_encoding.py
@@ -1,6 +1,5 @@
 import jax.numpy as jnp
 from encoding.bpe import BPETokenizer as student
-from encoding.sol import BPETokenizer as sol
 
 class TestEncoder():
     def __init__(self):
@@ -12,23 +11,23 @@ class TestEncoder():
     
     def test_encoder(self):
         student_encoder = student()
-        solution_encoder = sol()
 
         text = "Hello, this is a test of the min-GPT tokenizer :) returns TRUE if 100% of your code is correct!"
+        
         actual_encoded = student_encoder(text)
-        expected_encoded = solution_encoder(text)
+        expected_encoded = jnp.array([15496, 11, 428, 318, 257, 1332, 286, 262, 949, 12, 38, 11571, 11241, 7509, 14373, 5860, 26751, 611, 1802, 4, 286, 534, 2438, 318, 3376, 0])
 
         assert jnp.allclose(actual_encoded, expected_encoded)
 
     def test_decoder(self):
         student_encoder = student()
-        solution_encoder = sol()
 
         text = "Hello, this is a test of the min-GPT tokenizer :) returns TRUE if 100% of your code is correct!"
+
         encoded = student_encoder(text)
-
         actual_decoded = student_encoder.decode(encoded)
-        expected_decoded = solution_encoder.decode(encoded)
 
-        assert actual_decoded == expected_decoded
+        assert actual_decoded == text
  
+if __name__ == "__main__":
+    TestEncoder().autograde()
